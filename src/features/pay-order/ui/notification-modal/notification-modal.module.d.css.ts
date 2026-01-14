34b50dbaba9ff5228066a8317181d39ec8485469
vissarov.vasiliy <vissarov.vasiliy@rwb.ru>
2026-01-14 03:00:58 +0300

    npm audit

--- src/features/pay-order/ui/notification-modal/notification-modal.module.d.css.ts
+++ src/features/pay-order/ui/notification-modal/notification-modal.module.d.css.ts
@@ -1,8 +1,8 @@
 declare const classNames: {
-  readonly notification__container: 'notification__container';
-  readonly notification__title: 'notification__title';
-  readonly notification__order_id: 'notification__order_id';
-  readonly notification__description: 'notification__description';
-  readonly description_action: 'description_action';
+  readonly notification__container: "notification__container";
+  readonly notification__title: "notification__title";
+  readonly notification__order_id: "notification__order_id";
+  readonly notification__description: "notification__description";
+  readonly description_action: "description_action";
 };
 export default classNames;
